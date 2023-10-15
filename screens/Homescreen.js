3350a5b3d55ecc985f8745fe1fda399f4f09ab6e
PrattayDhar <prattaygub@gmail.com>
2023-10-15 17:35:01 +0600

    slider

--- screens/Homescreen.js
+++ screens/Homescreen.js
@@ -6,15 +6,18 @@ import * as Icon from "react-native-feather";
 
 export default function Homescreen() {
   return (
-     <SafeAreaView style={{ flex: 1, backgroundColor: 'white' }}>
+    <SafeAreaView style={{ flex: 1, backgroundColor: 'white' }}>
       <StatusBar barStyle="dark-content" />
-      <View style={{ paddingHorizontal: 16, paddingVertical: 8 }}>
-        <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between', padding: 8, borderRadius: 24, borderWidth: 1, borderColor: 'gray' }}>
+      <View style={{ paddingHorizontal: 18, paddingVertical: 7, flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }}>
+        <View style={{ flexDirection: 'row', alignItems: 'center', padding: 8, borderRadius: 20, borderWidth: 1, borderColor: 'gray' }}>
           <Icon.Search height={25} width={25} stroke="gray" />
           <TextInput
             style={{ flex: 1, marginLeft: 8, fontSize: 16 }}
             placeholder='Restaurants'
           />
+        </View>
+        <View style={{ padding: 8, borderRadius: 24, backgroundColor: 'gray', alignItems: 'center', justifyContent: 'center' }}>
+          <Icon.Sliders height="20" width="20" strokeWidth={2.5} stroke="white" />
         </View>
       </View>
     </SafeAreaView>
