6c7c5d29c9971b63dcbac916ef03da6e9097662c
PrattayDhar <prattaygub@gmail.com>
2023-10-18 14:59:48 +0600

    Cartscreen

--- screens/CartScreen.js
+++ screens/CartScreen.js
@@ -1,5 +1,5 @@
-import { View, Text, TouchableOpacity } from "react-native";
 import React from "react";
+import { View, Text, TouchableOpacity, Image, ScrollView } from "react-native";
 import * as Icon from "react-native-feather";
 import { useNavigation } from "@react-navigation/native";
 import { themeColors } from "../theam";
@@ -10,7 +10,7 @@ export default function CartScreen() {
   const navigation = useNavigation();
 
   return (
-    <View>
+    <View style={{ flex: 1 }}>
       <View style={{ position: "relative" }}>
         <TouchableOpacity
           onPress={() => navigation.goBack()}
@@ -28,10 +28,162 @@ export default function CartScreen() {
         >
           <Icon.ArrowLeft strokeWidth={3} stroke="white" />
         </TouchableOpacity>
-        <Text style={{ textAlign: "center", fontSize: 24, fontWeight: "bold" }}>
+        <Text
+          style={{
+            textAlign: "center",
+            fontSize: 24,
+            fontWeight: "bold",
+            marginTop: 50,
+          }}
+        >
           Your Cart
         </Text>
-        
+        <Text
+          style={{ textAlign: "center", fontSize: 15, fontWeight: "light" }}
+        >
+          {restaurant.name}
+        </Text>
+      </View>
+      <View>
+        {/* Delivery Time */}
+        <View
+          style={{
+            backgroundColor: themeColors.bgColor(0.3),
+            flexDirection: "row",
+            alignItems: "center",
+            paddingHorizontal: 10,
+          }}
+        >
+          <Image
+            source={require("../assets/images/bikeGuy.png")}
+            style={{ width: 80, height: 80, borderRadius: 40 }}
+          />
+          <Text style={{ flex: 1, paddingLeft: 10 }}>
+            Deliver In 30-40 Mins
+          </Text>
+          <TouchableOpacity>
+            <Text style={{ fontWeight: "bold", color: themeColors.text }}>
+              Change
+            </Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ paddingBottom: 50 }}
+      >
+        {restaurant.dishes.map((dish, index) => (
+          <View
+            key={index}
+            style={{
+              flexDirection: "row",
+              alignItems: "center",
+              marginVertical: 2,
+              paddingHorizontal: 10,
+            }}
+          >
+            <Text
+              style={{
+                fontSize: 18,
+                fontWeight: "bold",
+                color: themeColors.text,
+              }}
+            >
+              {" "}
+              2 x
+            </Text>
+            <Image
+              style={{
+                width: 80,
+                height: 80,
+                borderRadius: 40,
+              }}
+              source={dish.image}
+            />
+            <Text style={{ flex: 1, paddingLeft: 10, fontWeight: "bold" }}>
+              {dish.name}
+            </Text>
+            <Text style={{ fontWeight: "bold", fontSize: 18 }}>
+              ${dish.price}
+            </Text>
+            <TouchableOpacity
+              style={{
+                padding: 10,
+                borderRadius: 50,
+                backgroundColor: themeColors.bgColor(1),
+              }}
+            >
+              <Icon.Minus
+                strokeWidth={2}
+                height={20}
+                width={20}
+                stroke="white"
+              />
+            </TouchableOpacity>
+          </View>
+        ))}
+      </ScrollView>
+      {/* Calculation  */}
+      <View
+        style={{
+          backgroundColor: themeColors.bgColor(0.2),
+          paddingHorizontal: 10,
+          paddingTop: 20,
+          paddingBottom: 20,
+          borderRadius: 20,
+        }}
+      >
+        <View
+          style={{
+            paddingBottom: 20,
+            flexDirection: "row",
+            justifyContent: "space-between",
+            alignItems: "center",
+          }}
+        >
+          <Text style={{ flex: 1, color: themeColors.text }}>Subtotal</Text>
+          <Text style={{ color: themeColors.text }}>$20</Text>
+        </View>
+        <View
+          style={{
+            paddingBottom: 20,
+            flexDirection: "row",
+            justifyContent: "space-between",
+            alignItems: "center",
+          }}
+        >
+          <Text style={{ flex: 1, color: themeColors.text }}>Delivery Fee</Text>
+          <Text style={{ color: themeColors.text }}>$2</Text>
+        </View>
+        <View
+          style={{
+            paddingBottom: 10,
+            flexDirection: "row",
+            justifyContent: "space-between",
+            alignItems: "center",
+          }}
+        >
+          <Text style={{ flex: 1, color: themeColors.text }}>Order Total</Text>
+          <Text style={{ color: themeColors.text }}>$22</Text>
+        </View>
+        <TouchableOpacity
+          style={{
+            backgroundColor: themeColors.bgColor(1),
+            borderRadius: 50,
+            padding: 10,
+          }}
+        >
+          <Text
+            style={{
+              color: "white",
+              textAlign: "center",
+              fontWeight: "bold",
+              fontSize: 18,
+            }}
+          >
+            Place Order
+          </Text>
+        </TouchableOpacity>
       </View>
     </View>
   );
